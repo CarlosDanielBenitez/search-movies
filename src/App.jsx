a59c995034b06be29deba1ffa6b5aa16512cf823
CarlosDanielBenitez <cdanibenz@gmail.com>
2024-03-07 17:54:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <h2>main component</h2>
+  )
+}
+
+export default App
+// https://www.omdbapi.com/?apikey=a1a43ee&s=troya
+// https://www.omdbapi.com/?apikey=a1a43ee&i=troya
